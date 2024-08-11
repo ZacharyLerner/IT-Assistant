30ffdecb9b7887001c44a7dec0fa5607d2e352a4
ZacharyLerner <zacharynlerner@gmail.com>
2024-08-11 13:33:28 -0400

    File Processor Finalized
    Added documentation and final intigration for all file types to the updater

--- file_management/updater.py
+++ file_management/updater.py
@@ -1,22 +1,26 @@
 import os
-from html_cleaner import clean
+from html_cleaner import clean_and_format_file
 from text_cleaner import write_json
 
-html_output = "file_management/cleaned_html.txt"
-json_output = "file_management/data.json"
+# files chosen to manage the file cleaning and json writing, can be changed 
+file_output = "file_management/cleaned_html.txt"
+json_output = "file_management/data2.json"
 
-# assign directory
-directory = 'file_management/html_files'
+# Runs and write all files to a json file, great for updating but can have inaccuracies 
+def update_files(directory):
+    # assign directory
+    # ensure the output text file is empty before processing
+    open(file_output, 'w').close()
 
-# ensure the output text file is empty before processing
-open(html_output, 'w').close()
+    # iterate over files in that directory
+    for index, filename in enumerate(os.scandir(directory)):
+        if filename.is_file():
+            print("File processed:", filename.path)
+            mode = 'w' if index == 0 else 'a'  # use write mode for the first file, append mode for the rest
+            clean_and_format_file(filename.path, file_output, mode)
 
-# iterate over files in that directory
-for index, filename in enumerate(os.scandir(directory)):
-    if filename.is_file():
-        print("File processed:", filename.path)
-        mode = 'w' if index == 0 else 'a'  # use write mode for the first file, append mode for the rest
-        clean(filename.path, html_output, mode)
+    write_json(file_output, json_output)
+    print("File written to JSON")
 
-write_json(html_output, json_output)
-print("File written to JSON")
+# run a file directory to create a full json of those files, this can be manually view and edited to help assist search
+update_files("file_management/google_docs")
